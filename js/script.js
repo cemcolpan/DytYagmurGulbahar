e81167475feb7a347d7ac4053fb82eec5d11f86e
Cem Colpan <cemcolpan@gmail.com>
2022-08-30 23:20:41 +0300

    Swiper Slider & Instagram tool

--- js/script.js
+++ js/script.js
@@ -7,3 +7,87 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+
+var swiper = new Swiper(".home-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  autoHeight:true,
+  spaceBetween: 20,
+  pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+  },
+  breakpoints: {
+     0: {
+       slidesPerView: 1,
+     },
+     700: {
+       slidesPerView: 2,
+     },
+     1000: {
+       slidesPerView: 3,
+     },
+  },
+});
+
+var swiper = new Swiper(".instagram-slider", {
+  loop:true,
+  centeredSlides:true,
+  autoHeight:true,
+  spaceBetween: 20,
+  pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+  },
+  breakpoints: {
+     0: {
+       slidesPerView: 1,
+     },
+     700: {
+       slidesPerView: 2,
+     },
+     1000: {
+       slidesPerView: 3,
+     },
+     1300: {
+      slidesPerView: 4,
+    },
+    1500: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+var swiper = new Swiper(".comments-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  spaceBetween: 20,
+  pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+  },
+  breakpoints: {
+     0: {
+       slidesPerView: 1,
+     },
+     700: {
+       slidesPerView: 2,
+     },
+     1000: {
+       slidesPerView: 3,
+     },
+  },
+});
